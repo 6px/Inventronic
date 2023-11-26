0c62ddf8ebd1fdd747ae86361f2d7e020dd4159a
Jonathan Foucher <jfoucher@gmail.com>
2023-11-26 19:55:27 +0100

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  target: 'static',
   modules: ['@nuxtjs/supabase', '@nuxt/ui', '@nuxtjs/color-mode'],
   runtimeConfig: {
     public: {
