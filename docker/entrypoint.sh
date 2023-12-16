79eaa3f6f6ff65e55da2033a54101a3e28a31914
Jonathan Foucher <jfoucher@gmail.com>
2023-12-16 16:36:19 +0100

    again

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+trap 'pkill -P $$; exit 1;' TERM INT
+
 sed -i 's|APP_BASE_URL|'$BASE_URL'|g' /app/server/chunks/nitro/node-server.mjs
 sed -i 's/SUPABASE_KEY/'$SUPABASE_KEY'/g' /app/server/chunks/nitro/node-server.mjs
 if [ -n "$SERVER_URL" ]; then
