812eafd2edd57be00d2261c12bdae23998b3204c
Jonathan Foucher <jfoucher@gmail.com>
2023-12-16 18:13:35 +0100

    .

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 trap 'pkill -P $$; exit 1;' TERM INT
 
-if [ "$SERVER_URL" = "SERVER_URL" ]; then
-    $SERVER_URL = $SUPABASE_URL
-fi
+
 
 echo "server url $SERVER_URL"
 
 sed -i 's|APP_BASE_URL|'$BASE_URL'|g' /app/server/chunks/nitro/node-server.mjs
 sed -i 's/SUPABASE_KEY/'$SUPABASE_KEY'/g' /app/server/chunks/nitro/node-server.mjs
 if [ -n "$SERVER_URL" ]; then
+    if [ "$SERVER_URL" = "SERVER_URL" ]; then
+        echo "changing server url $SERVER_URL"   
+        SERVER_URL = $SUPABASE_URL
+    fi
     sed -i 's|SERVER_URL|'$SERVER_URL'|g' /app/server/chunks/nitro/node-server.mjs
 else
     sed -i 's|SERVER_URL|'$SUPABASE_URL'|g' /app/server/chunks/nitro/node-server.mjs
